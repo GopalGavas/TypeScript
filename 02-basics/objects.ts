75d112ada120bdb2334ee2e6bd3f39ed27e04a49
GopalGavas <gopalgavas25@outlook.com>
2024-12-18 14:44:10 +0530

    add type aliases

--- 02-basics/objects.ts
+++ 02-basics/objects.ts
@@ -15,3 +15,16 @@ function createCourse(): { name: string; price: number } {
   return { name: "reactjs", price: 399 };
 }
 */
+
+// Type Aliases
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function createUser(user: User): User {
+  return user;
+}
+
+createUser({ name: "Gopal", email: "gopal7@dev.com", isActive: true });
