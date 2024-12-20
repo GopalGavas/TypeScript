a1636bec9f4627e785e22a1ac32d2fd8168fd67a
GopalGavas <gopalgavas25@outlook.com>
2024-12-20 12:47:11 +0530

    add classes , getters and setters

---
+++ 03-setup/dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+    constructor(email, name // private userId: string
+    ) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get appleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+const gopal = new User("gopalgavas@gmail.com", "Gopal");
+// gopal.city = "Mumbai";
