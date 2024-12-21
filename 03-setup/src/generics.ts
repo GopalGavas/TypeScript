d6dfbad00f9b5a662ff91d9fcc7fd6f0cfc65a9b
GopalGavas <gopalgavas25@outlook.com>
2024-12-21 13:51:34 +0530

    add generics

---
+++ 03-setup/src/generics.ts
@@ -0,0 +1,77 @@
+const score: Array<number> = [];
+const names: Array<string> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+  return val;
+}
+
+function identityTwo(val: any): any {
+  return val;
+}
+
+function identityThree<Type>(val: Type): Type {
+  return val;
+}
+
+// identityThree(true)
+
+function identityFour<T>(val: T): T {
+  return val;
+}
+
+interface Bottle {
+  brand: string;
+  type: number;
+}
+
+// identityFour<Bottle>({});
+
+function getSearchProducts<T>(products: T[]): T {
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getMoreSearchProducts = <T>(products: T[]) => {
+  // do some database operations
+  const myIndex = 4;
+  return products[myIndex];
+};
+
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U) {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+// anotherFunction(3, {
+//   connection: "dataDb connection",
+//   username: "Gopal25",
+//   password: "Gopal123",
+// });
+
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = [];
+
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
